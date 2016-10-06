a05fe79fc3f040a872a34b1fbf925fa93dd48d15
Vladimir Pavlov <kolobokhtc@gmail.com>
2016-10-06 12:02:12 +0300

    make AuthGuard Injectable

--- app/core/auth-guard.service.ts
+++ app/core/auth-guard.service.ts
@@ -1,19 +1,24 @@
 import {AuthService} from "./auth.service";
-import {Router, CanActivate} from "@angular/router";
+import {Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from "@angular/router";
+import {Observable} from "rxjs";
+import {Injectable} from "@angular/core";
 /**
  * Created by Vladimir Pavlov (kolobokhtc@gmail.com) on 06.10.2016.
  */
 
+@Injectable()
 export class AuthGuard implements CanActivate{
 
-    canActivate(): boolean {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>|Promise<boolean>|boolean {
         if (!this.auth.loggedIn()){
-            this.router.navigate(['']);
+            this.router.navigate(['login']);
             return false;
         }
 
         return true;
     }
+
+
 
     constructor(private auth: AuthService, private router: Router){
 
